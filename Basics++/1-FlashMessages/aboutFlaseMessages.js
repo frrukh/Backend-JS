cf453d20e719ff4a2bd0a4d212812f8df64d8769
frrukh <mhmdfrrukh13@gmail.com>
2024-04-01 22:44:39 +0500

    starting basics++ topics

---
+++ Basics++/1-FlashMessages/aboutFlaseMessages.js
@@ -0,0 +1,9 @@
+// Flash Messages:
+
+// setup flash messages:
+    // flash messages works upon sessions so it is essential to install and setup express-session first:
+        // command : npm i express-session
+    // after that we need to install connect-flash package in order to use flash messages.
+        // command : npm i connect-flash
+    // after this we need to setup connect-flash in app.js file.
+    // NOW we are ready to use flash messages.
